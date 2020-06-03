00e9717da83ea11d6bf7740ae886fa1e9e59c164
Nataliya <n.jelonkina@mail.ru>
2020-06-03 21:11:38 +0300

    fifth + slider

--- src/scripts/owl.js
+++ src/scripts/owl.js
@@ -1,11 +1,25 @@
+// $(document).ready(function(){
+//   $('.owl-carousel').owlCarousel({
+//     items: 1,
+//     nav:false,
+//     loop:true,
+//     dots: true,
+//     dotsClass: 'container-dots',
+//     dotClass: 'circle-btn'
+//   });
+// });
+
+
+
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     items: 1,
+    center:true,
     nav:false,
     loop:true,
     dots: true,
     dotsClass: 'container-dots',
     dotClass: 'circle-btn'
+
   });
 });
-
