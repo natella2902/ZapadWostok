40b71962514c0a7924f5ed8ff5508cd9199b14f4
Nataliya <n.jelonkina@mail.ru>
2020-06-03 16:21:23 +0300

    slider test

--- gulp.config.js
+++ gulp.config.js
@@ -3,12 +3,14 @@ module.exports = {
   DIST_PATH: "dist",
   STYLES_LIBS: [
     'node_modules/normalize.css/normalize.css',
-    'node_modules/bxslider/dist/jquery.bxslider.css'
+    'node_modules/bxslider/dist/jquery.bxslider.css',
+    'node_modules/owl.carousel/dist/assets/owl.carousel.css'
 
   ],
   JS_LIBS: [
-    'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery/dist/jquery.min.js',
     'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js',
-    'node_modules/bxslider/dist/jquery.bxslider.min.js'
+    'node_modules/bxslider/dist/jquery.bxslider.min.js',
+    'node_modules/owl.carousel/dist/owl.carousel.min.js'
   ]
 }
---
+++ src/scripts/owl.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    nav:false,
+    loop:true,
+    dots: true,
+    dotsClass: 'container-dots',
+    dotClass: 'circle-btn'
+  });
+});
+
