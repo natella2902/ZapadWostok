14c6bc2245ef137d5c308615c5f8a4db35ba1929
Nataliya <n.jelonkina@mail.ru>
2020-06-04 09:15:53 +0300

    devided slider

---
+++ src/scripts/owlArrows.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.owlArrows').owlCarousel({
+    items: 1,
+    center:true,
+    nav:true,
+    loop:true,
+    dots: false,
+    dotsClass: 'container-dots',
+    dotClass: 'circle-btn'
+
+  });
+});
---
+++ src/scripts/owlDots.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.owlDots').owlCarousel({
+    items: 1,
+    center:true,
+    nav:false,
+    loop:true,
+    dots: true,
+    dotsClass: 'container-dots',
+    dotClass: 'circle-btn'
+
+  });
+});
